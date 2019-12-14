4f5246c80a6fd15d98fc4c68eff79124b4216d91
Tyler Horth <tylerhorth@outlook.com>
2019-12-13 23:16:39 -0800

    day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,13 +1,30 @@
 use aoc_runner_derive::aoc;
 use crate::intcode::computer::Computer;
 
-#[aoc(day2, part1)]
-fn run_program(program: &str) -> i64 {
+fn execute(program: &str, a: i64, b: i64) -> i64 {
     let mut computer = Computer::load(program).unwrap();
 
-    computer.set(1, 12).unwrap();
-    computer.set(2, 2).unwrap();
+    computer.set(1, a).unwrap();
+    computer.set(2, b).unwrap();
     computer.run().unwrap();
 
     computer.get(0).unwrap()
+}
+
+#[aoc(day2, part1)]
+fn run_program(program: &str) -> i64 {
+    execute(program, 12, 2)
+}
+
+#[aoc(day2, part2)]
+fn find_values(program: &str) -> i64 {
+    for i in 0..=99 {
+        for j in 0..=99 {
+            if execute(program, i, j) == 19690720 {
+                return 100 * i + j
+            }
+        }
+    }
+
+    panic!("No noun-verb combination found");
 }
